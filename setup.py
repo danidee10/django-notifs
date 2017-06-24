1f95a6f057f7eeba6feb793add8ff6d4e94b5a5e
Osaetin Daniel <osaetindaniel@gmail.com>
2017-06-24 01:40:27 +0100

    Recursively added all files

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup
 
+def package_files(directory):
+    """Recursively add subfolders and files."""
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = package_files('notifications')
+
 setup(name='django-notifs',
-      version='1.2',
+      version='1.3',
       description='Re-usable notification app for Django',
       url='https://github.com/danidee10/django-notifs',
       author='Osaetin Daniel',
       author_email='osaetindaniel@gmail.com',
       license='GPL',
       packages=['notifications'],
+      package_data={'': extra_files},
       install_requires=[
           'django',
       ],
