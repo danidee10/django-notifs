107ed9b352527e381bbff7baa049e9f3fe6d16d3
Osaetin Daniel <osaetindaniel@gmail.com>
2017-07-18 19:18:54 +0100

    Bumped version up to 1.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def package_files(directory):
 extra_files = package_files('notifications')
 
 setup(name='django-notifs',
-      version='1.3',
+      version='1.4',
       description='Re-usable notification app for Django',
       url='https://github.com/danidee10/django-notifs',
       author='Osaetin Daniel',
