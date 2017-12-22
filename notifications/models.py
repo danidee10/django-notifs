c2999cfcb87c797dd9aaa10912466f8740eaef2b
Osaetin Daniel <osaetindaniel@gmail.com>
2017-12-22 11:57:34 +0100

    Removed notification adapters from settings
    Changed User model to get_user_model to make it more compatible with
    other apps that don't use the default Django user model

--- notifications/models.py
+++ notifications/models.py
@@ -1,7 +1,7 @@
 """Notification model."""
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
 class NotificationQuerySet(models.QuerySet):
@@ -19,28 +19,52 @@ class NotificationQuerySet(models.QuerySet):
 
 
 class Notification(models.Model):
-    """Model for notifications."""
+    """
+    Model for notifications.
+
+    Parameters:
+    ----------
+    source: A ForeignKey to Django's User model
+        (Can be null if it's not a User to User Notification)
+
+    source_display_name: A User Friendly name
+        for the source of the notification.
+
+    recipient: The Recipient of the notification.
+        It's a ForeignKey to Django's User model.
+
+    action: Verbal action for the notification
+        E.g Sent, Cancelled, Bought e.t.c
+
+    obj: The id of the object associated with the notification
+        (Can be null)
+
+    short_description: The body of the notification.
+
+    url: The url of the object associated with the notification
+        (Can be null)
+    """
+
+    User = get_user_model()
 
     class Meta:
-        """Specify ordering for objects."""
+        """Specify ordering for notifications."""
         ordering = ('-id',)
 
     source = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     source_display_name = models.CharField(max_length=150, null=True)
     recipient = models.ForeignKey(
         User, related_name='notifications', on_delete=models.CASCADE
-        )
-    action = models.CharField(max_length=50)  # e.g 'Created'
+    )
+    action = models.CharField(max_length=50)
     category = models.CharField(max_length=50)
-    obj = models.IntegerField()  # id of the object
-    url = models.URLField()  # url of the object (E.g calling obj.absolute_url)
+    obj = models.IntegerField(null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    short_description = models.CharField(max_length=100)
     is_read = models.BooleanField(default=False)
 
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
-
-    # e.g 'a New Quote'
-    short_description = models.CharField(max_length=100)
 
     # register queryset
     objects = NotificationQuerySet.as_manager()
