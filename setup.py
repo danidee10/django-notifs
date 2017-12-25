b2728c9ee19851f3f764da34e0ec0d3807ba3220
Osaetin Daniel <osaetindaniel@gmail.com>
2017-12-25 14:23:46 +0100

    2.3 Release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def package_files(directory):
 extra_files = package_files('notifications')
 
 setup(name='django-notifs',
-      version='2.2',
+      version='2.3',
       description='Re-usable notification app for Django',
       url='https://github.com/danidee10/django-notifs',
       author='Osaetin Daniel',
