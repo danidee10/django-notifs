109c49b77793ec62192d67511ffae30335e0d106
Osaetin Daniel <osaetindaniel@gmail.com>
2019-08-12 19:52:12 -0400

    Refactor utils

--- notifications/utils.py
+++ notifications/utils.py
@@ -1,37 +1,12 @@
 """Utilities and helper functions."""
 
-import importlib
-
 from . import NotificationError
 from .models import Notification
-from . import default_settings as settings
-
-
-def import_channel(channel_alias):
-    """
-    helper to import channels aliases from string paths.
-
-    raises an AttributeError if a channel can't be found by it's alias
-    """
-
-    try:
-        channel_path = settings.NOTIFICATIONS_CHANNELS[channel_alias]
-    except KeyError:
-        raise AttributeError(
-            '"%s" is not a valid delivery channel alias. '
-            'Check your applications settings for NOTIFICATIONS_CHANNELS'
-            % channel_alias
-        )
-
-    package, attr = channel_path.rsplit('.', 1)
-
-    return getattr(importlib.import_module(package), attr)
+from .tasks import send_notification
 
 
 def notify(silent=False, **kwargs):
     """Helper method to send a notification."""
-    from .tasks import send_notification
-
     notification = Notification(**kwargs)
 
     # If it's a not a silent notification, save the notification
