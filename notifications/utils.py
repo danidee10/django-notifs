a22e0fe4252e215470b560d5beefe5b87a35f8ab
Daniel Osaetin <osaetindaniel@gmail.com>
2021-02-16 23:47:26 +0100

    Create celery backend

--- notifications/utils.py
+++ notifications/utils.py
@@ -1,8 +1,10 @@
 """Utilities and helper functions."""
 
+import importlib
+
 from . import NotificationError
 from .models import Notification
-from .tasks import send_notification, __validate_channel_alias
+from . import default_settings as settings
 
 
 def notify(silent=False, **kwargs):
@@ -11,14 +13,17 @@ def notify(silent=False, **kwargs):
 
     # Validate channels
     for channel_alias in notification.channels:
-        __validate_channel_alias(channel_alias)
+        _validate_channel_alias(channel_alias)
 
     # If it's a not a silent notification, save the notification
     if not silent:
         notification.save()
 
     # Send the notification asynchronously with celery
-    send_notification.delay(notification.to_json())
+    notification_delivery_backend = _import_class_string(
+        settings.NOTIFICATIONS_DELIVERY_BACKEND
+    )
+    notification_delivery_backend(notification).run()
 
 
 def read(notify_id, recipient):
@@ -34,3 +39,29 @@ def read(notify_id, recipient):
         raise NotificationError('You cannot read this notification')
 
     notification.read()
+
+
+def _validate_channel_alias(channel_alias):
+    """
+    Validates a channel alias against settings.NOTIFICATION_CHANNELS.
+
+    returns the channel's path (i.e the path to the Channel's class)
+    raises an AttributeError for invalid aliases
+    """
+    try:
+        channel_path = settings.NOTIFICATIONS_CHANNELS[channel_alias]
+    except KeyError:
+        raise AttributeError(
+            '"%s" is not a valid delivery channel alias. '
+            'Check your applications settings for NOTIFICATIONS_CHANNELS'
+            % channel_alias
+        )
+
+    return channel_path
+
+
+def _import_class_string(path):
+    """helper to import classes from string paths."""
+    package, attr = path.rsplit('.', 1)
+
+    return getattr(importlib.import_module(package), attr)
