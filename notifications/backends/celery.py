21257808151531c35d5a9dc389c5b903c1727131
Daniel Osaetin <osaetindaniel@gmail.com>
2021-02-23 12:16:35 +0100

    Add retry functionality

--- notifications/backends/celery.py
+++ notifications/backends/celery.py
@@ -2,29 +2,36 @@
 
 from __future__ import absolute_import, unicode_literals
 
-from .. import default_settings as settings
 
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
 from .base import BaseBackend
 from .utils import _send_notification
+from .. import default_settings as settings
 
 
 logger = get_task_logger(__name__)
 
+autoretry_for = []
+if settings.NOTIFICATIONS_RETRY:
+    autoretry_for.append(Exception)
 
-@shared_task
+
+@shared_task(
+    autoretry_for=autoretry_for,
+    retry_backoff=settings.NOTIFICATIONS_RETRY_INTERVAL,
+    max_retries=settings.NOTIFICATIONS_MAX_RETRIES
+)
 def send_notification(notification, channel_alias):
     """Send notification via a channel to celery."""
     _send_notification(notification, channel_alias, logger)
 
 
 class CeleryBackend(BaseBackend):
-
     def run(self):
-        for channel_alias in self.notification['channels']:
+        for channel_alias in self.notification["channels"]:
             send_notification.apply_async(
                 args=[self.notification, channel_alias],
-                queue=settings.NOTIFICATIONS_QUEUE_NAME
+                queue=settings.NOTIFICATIONS_QUEUE_NAME,
             )
--- notifications/backends/rq.py
+++ notifications/backends/rq.py
@@ -2,16 +2,23 @@
 
 import logging
 
-from .. import default_settings as settings
-
 import django_rq
+from rq import Retry
 
 from .base import BaseBackend
 from .utils import _send_notification
+from .. import default_settings as settings
 
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('django_notifs.backends.rq')
+
+retry = None
+if settings.NOTIFICATIONS_RETRY:
+    retry = Retry(
+        max=settings.NOTIFICATIONS_MAX_RETRIES,
+        interval=settings.NOTIFICATIONS_RETRY_INTERVAL
+    )
 
 
 class RQBackend(BaseBackend):
@@ -20,5 +27,6 @@ class RQBackend(BaseBackend):
         for channel_alias in self.notification['channels']:
             queue = django_rq.get_queue(settings.NOTIFICATIONS_QUEUE_NAME)
             queue.enqueue(
-                _send_notification, self.notification, channel_alias, logger
+                _send_notification, self.notification, channel_alias, logger,
+                retry=retry
             )
