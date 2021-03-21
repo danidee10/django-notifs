23baeafdc08d19e4428595536990eb1d1c0dfd48
Daniel Osaetin <osaetindaniel@gmail.com>
2021-03-21 19:01:08 +0100

    fix: import errors for optional dependencies

--- notifications/backends/__init__.py
+++ notifications/backends/__init__.py
@@ -1,4 +1,18 @@
-from .rq import RQBackend as RQ  # noqa
-from .celery import CeleryBackend as Celery  # noqa
-from .django_channels import ChannelsBackend as Channels  # noqa
+# Optional dependencies are wrapped in try/excepts to prevent
+# ImportErrors if the user hasn't installed them
+# Surely, there's a better way to handle this :/
+
+try:
+    from .rq import RQBackend as RQ  # noqa
+except ImportError:
+    pass
+try:
+    from .celery import CeleryBackend as Celery  # noqa
+except ImportError:
+    pass
+try:
+    from .django_channels import ChannelsBackend as Channels  # noqa
+except ImportError:
+    pass
+
 from .synchronous import SynchronousBackend as Synchronous  # noqa
