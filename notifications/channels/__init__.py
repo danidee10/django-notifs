11d648fb311264725111551a74c83eb635958fc7
Daniel Osaetin <osaetindaniel@gmail.com>
2021-03-24 23:56:13 +0100

    Make websocket channel dependencies optional

--- notifications/channels/__init__.py
+++ notifications/channels/__init__.py
@@ -1,4 +1,7 @@
 from .console import ConsoleChannel  # noqa
 from .base import BaseNotificationChannel  # noqa
-from .channels_websocket import WebSocketChannel  # noqa
+try:
+    from .channels_websocket import WebSocketChannel  # noqa
+except ImportError:
+    pass
 from .push_notification import PushNotificationChannel  # noqa
