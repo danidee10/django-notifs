fcc8be31b1edfd97972ba45be5fd0cf4037c860f
Daniel Osaetin <osaetindaniel@gmail.com>
2021-09-26 23:27:39 +0200

    dep: Add pusher dependency

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ EXTRAS_REQUIRE = {
     'celery': ['celery>=4.1.0'],
     'rq': ['django-rq>=2.4.0'],
     'channels': ['channels>=3.0.3', 'channels-redis>=3.2.0'],
+    'pusher': ['pusher>=3.0.0'],
 }
 EXCLUDE = ['notifs', 'tests', '*.tests', '*.tests.*', 'tests.*']
 
