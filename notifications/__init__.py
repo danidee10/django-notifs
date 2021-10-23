ae9ce8b70a94c859c7fabcf51b2a2a7b5a32dedb
Osaetin Daniel <osaetindaniel@gmail.com>
2021-10-23 11:15:25 +0200

    feat: Handle missing provider dependencies (#87)
    feat: Handle missing provider dependencies
    
    * Undo fcm_web changes
    * Update twitter import
    * Remove travis-ci.yml, .pycodestyle
    *Configure pep8 for codeclimate

--- notifications/__init__.py
+++ notifications/__init__.py
@@ -7,4 +7,16 @@ class NotificationError(Exception):
     pass
 
 
+class ImproperlyInstalledProvider(ImportError):
+    """Exception for missing provider dependencies"""
+
+    def __init__(self, missing_package, provider):
+        message = (
+            f'The `{missing_package}` package is required to use this Provider, '
+            "but it isn't installed.\n"
+            f'(Be sure to use `pip install django-notifs[{provider}]`)'
+        )
+        super().__init__(message)
+
+
 default_app_config = 'notifications.apps.NotificationsConfig'
