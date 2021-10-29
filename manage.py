cdd122719ccea2d56b73b00603c8f82fded701f7
Daniel Osaetin <osaetindaniel@gmail.com>
2021-10-29 23:06:47 +0200

    Remove test_settings file

--- manage.py
+++ manage.py
@@ -3,10 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    if 'test' in sys.argv:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notifs.test_settings')
-    else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notifs.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notifs.settings")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
