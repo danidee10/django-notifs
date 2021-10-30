98a34d02085594d54d3e66a7362e89710171c9b9
Osaetin Daniel <osaetindaniel@gmail.com>
2021-10-30 06:27:39 +0200

    feat: Use pyFCM for fcm_web provider (#94)
    * fix: proxy error
    * fix: proxy argument

--- notifications/providers/__init__.py
+++ notifications/providers/__init__.py
@@ -1,32 +1,8 @@
 from .base import BaseNotificationProvider  # noqa
+from .django_channels import DjangoChannelsNotificationProvider  # noqa
+from .django_sms import DjangoSMSNotificationProvider  # noqa
 from .email import EmailNotificationProvider  # noqa
-
-try:
-    from .pusher_channels import PusherChannelsNotificationProvider  # noqa
-except ImportError:
-    pass
-
-try:
-    from .fcm_web import FCMWebNotificationProvider  # noqa
-except ImportError:
-    pass
-
-try:
-    from .django_channels import DjangoChannelsNotificationProvider  # noqa
-except ImportError:
-    pass
-
-try:
-    from .slack import SlackNotificationProvider  # noqa
-except ImportError:
-    pass
-
-try:
-    from .django_sms import DjangoSMSNotificationProvider  # noqa
-except ImportError:
-    pass
-
-try:
-    from .twitter_status_update import TwitterStatusUpdateNotificationProvider  # noqa
-except ImportError:
-    pass
+from .fcm_web import FCMWebNotificationProvider  # noqa
+from .pusher_channels import PusherChannelsNotificationProvider  # noqa
+from .slack import SlackNotificationProvider  # noqa
+from .twitter_status_update import TwitterStatusUpdateNotificationProvider  # noqa
--- notifications/providers/fcm_web.py
+++ notifications/providers/fcm_web.py
@@ -1,30 +1,57 @@
-"""Adpaters to send notifications through various meduiums."""
+from pydantic import BaseModel, conlist
 
-import requests
-from django.conf import settings
+try:
+    from pyfcm import FCMNotification
+
+    HAS_DEPENDENCIES = True
+except ImportError:
+    HAS_DEPENDENCIES = False
+
+from notifications import default_settings as settings
 
 from . import BaseNotificationProvider
+
+
+class BaseFCMSchema(BaseModel):
+    message_title: str
+    message_body: str
+
+
+class FCMWebSchema(BaseFCMSchema):
+    registration_id: str
+
+
+class BulkFCMWebSchema(BaseFCMSchema):
+    registration_ids: conlist(str, min_items=1)
 
 
 class FCMWebNotificationProvider(BaseNotificationProvider):
     """Google FCM Web Provider."""
 
     name = 'fcm_web'
-    package = None
+    package = 'pyfcm'
+
+    HAS_DEPENDENCIES = HAS_DEPENDENCIES
+
+    def __init__(self, context=dict()):
+        super().__init__(context=context)
+        self.fcm_client = FCMNotification(
+            api_key=settings.NOTIFICATIONS_FCM_WEB_API_KEY,
+            proxy_dict=settings.NOTIFICATIONS_FCM_WEB_PROXY,
+        )
+
+    def get_validator(self):
+        if self.context.get('bulk', False) is True:
+            return BulkFCMWebSchema
+
+        return FCMWebSchema
 
     def send(self, payload):
-        requests.post(
-            'https://fcm.googleapis.com/fcm/send',
-            json={
-                'notification': {
-                    'title': payload['title'],
-                    'body': payload['body'],
-                    'click_action': payload['click_action'],
-                    'icon': payload['icon'],
-                },
-                'to': payload['to'],
-            },
-            headers={
-                'Authorization': 'key={}'.format(settings.NOTIFICATIONS_FCM_WEB_KEY)
-            },
-        )
+        response = self.fcm_client.notify_single_device(**payload)
+        if response['failure'] != 0:
+            self.logger.error(response)
+
+    def send_bulk(self, payloads):
+        response = self.fcm_client.notify_multiple_devices(**payloads)
+        if response['failure'] != 0:
+            self.logger.error(response)
